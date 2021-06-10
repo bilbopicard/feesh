5180a9f99293aeaa1c94d2a5d9e69b51eacf507d
James Sullivan <jsullivan86@gmail.com>
2021-06-09 22:02:29 -0700

    styled find appointment

--- react-app/src/components/FindAppointments.js
+++ react-app/src/components/FindAppointments.js
@@ -12,6 +12,28 @@ function FindAppointments() {
         return state.appointments.list.map(appointmentId => state.appointments[appointmentId]).filter(appointment => appointment.user_id !== userId)
     })
 
+    const niceDateFormat = (aptDate) => {
+        let d = new Date(aptDate),
+            month = '' + (d.getMonth() + 1),
+            day = '' + d.getDate(),
+            year = d.getFullYear(),
+            hh = d.getHours(),
+            m = d.getMinutes();
+
+        let dd = "AM";
+        let h = hh;
+        if (h >= 12) {
+            h = hh - 12;
+            dd = "PM";
+        }
+        if (h === 0) {
+            h = 12;
+        }
+        m = m < 10 ? "0" + m : m;
+
+        return [[month, day, year].join('-')];
+    }
+
     useEffect(() => {
         dispatch(displayAppointments())
     }, [dispatch])
@@ -20,21 +42,25 @@ function FindAppointments() {
     return (
         <>
             <div className='nav-empty-div'></div>
+            <h1 id='find-feeshing-h1'>Find Feeshing Trips</h1>
             <div id='find-appointment-container'>
                 <div id='find-appointment-left-div'>
                     {otherUserAppointments.map(appointment => (
-                        <NavLink to={`/appointments/${appointment.id}`} key={appointment.id}>
-                            <div className={`find-appointment-inner-div ${appointment.completed} ? 'completed' : ''}`} key={appointment.id}>
-                                <div className='find-appointment-inner-text'>
-                                    <p>Type of appointment: {appointment.appointment_type}</p>
-                                    <br />
-                                    <p>Description: {appointment.description}</p>
+                        <div id='find-appointment-left-inner'>
+                            <NavLink to={`/appointments/${appointment.id}`} key={appointment.id}>
+                                <div className={`find-appointment-inner-div ${appointment.completed} ? 'completed' : ''}`} key={appointment.id}>
+                                    <div className='find-appointment-inner-text'>
+                                        <p>Type of appointment: {appointment.appointment_type}</p>
+                                        <p>Description: {appointment.description}</p>
+                                        <p>Date: {niceDateFormat(appointment.date)}</p>
+                                        <p>Type of fish: {appointment.fish_type}</p>
+                                    </div>
+                                    <div id='find-appointment-image-div'>
+                                        <img src={appointment.image_url} alt="" />
+                                    </div>
                                 </div>
-                                <div id='find-appointment-image-div'>
-                                    <img src={appointment.image_url} alt="" />
-                                </div>
-                            </div>
-                        </NavLink>
+                            </NavLink>
+                        </div>
                     ))}
                 </div>
                 <div id='find-appointment-right-div'>
