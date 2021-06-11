c988fe0db3e3635988dd56b12e809081c188b9df
James Sullivan <jsullivan86@gmail.com>
2021-06-11 16:14:48 -0700

    added stuff

--- react-app/src/components/GetCertified.js
+++ react-app/src/components/GetCertified.js
@@ -20,7 +20,7 @@ function GetCertified() {
             <div className='nav-empty-div'></div>
             <div id='get-certified-container'>
                 <h1>Become a Certified Feesher!</h1>
-                <p>Do you love fish? Do you care about fish? Do you want to make some extra money taking care of fish? If these things are true you just need to become "Feesh certfied" and you can see needy people all around you that want help taking care of their fish. Watch the following video and you can become a "Feesher" and can become a bigger part of the Feesh community.</p>
+                <p>Do you love fish? Do you care about fish? Do you want to make some extra money taking care of fish? If these things are true you just need to become "Feesh certified" and you can see needy people all around you that want help taking care of their fish. Watch the following video and you can become a "Feesher" and can become a bigger part of the Feesh community.</p>
                 <div id='feed-video'>
                     <iframe width="560" height="315" src="https://www.youtube.com/embed/ti2En_UfnoE" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
                 </div>
